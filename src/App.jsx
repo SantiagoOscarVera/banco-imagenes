86fef0a209ec569bc9f403ae6b25d222df0bb51d
SantiagoOscarVera <santiagovera280@gmail.com>
2023-09-05 17:38:29 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <a href="https://github.com/SantiagoOscarVera" target="_blank">
+          <img src="https://media.discordapp.net/attachments/1148700056879386667/1148705975243649065/Foto_nueva.jpg?width=530&height=427" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://github.com/SantiagoOscarVera" target="_blank">
+          <img src="https://media.discordapp.net/attachments/1148700056879386667/1148705975507886171/github.png?width=427&height=427" className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>GitHub + Santiago Vera</h1>
+
+      <div>
+        <a href="https://www.linkedin.com/in/santiago-vera-programador-developer/" target="_blank">
+          <img src="https://media.discordapp.net/attachments/1148700056879386667/1148705975948292147/yo_linkedin.jpg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://www.linkedin.com/in/santiago-vera-programador-developer/" target="_blank">
+          <img src="https://media.discordapp.net/attachments/1148700056879386667/1148705975759536189/linkedin_logo.png?width=427&height=427" className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Linkedin + Santiago Vera</h1>
+      
+    </>
+  )
+}
+
+export default App
